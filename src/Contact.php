565041552d9e7f4ac6dd64f97398e9be3986efe5
risetechapps <apps@risetech.com.br>
2025-05-02 15:59:28 -0300

    implemtando suporte de set e get

--- src/Contact.php
+++ src/Contact.php
@@ -4,5 +4,15 @@ namespace RiseTechApps\Contact;
 
 class Contact
 {
+    protected static array $contact = [];
 
+    public static function setContact(array $contact): void
+    {
+        static::$contact = $contact;
+    }
+
+    public static function getContact(): array
+    {
+        return static::$contact;
+    }
 }
